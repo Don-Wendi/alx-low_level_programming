f569016bc06f78b66d1268a2d234360e1adc7ca3
Don-wendi <wendimusitotaw3@gmail.com>
2023-02-17 21:58:29 +0300

    solve 9

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,7 +2,9 @@
  * File: 100-print_comb3.c
  * Auth: Brennan D Baraban
  */
+
 #include <stdio.h>
+
 /**
  * main - Prints all possible combinations of two different digits,
  *        in ascending order, separated by a comma followed by a space.
@@ -22,6 +24,7 @@ int main(void)
 
 			if (digit1 == 8 && digit2 == 9)
 				continue;
+			
 			putchar(',');
 			putchar(' ');
 		}
