22fb28b46b6bc3f4d498d921f9f46c4fc7d8462c
wendi-code <wendimusit@gmail.com>
2023-02-24 22:11:22 +0300

    fix 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,12 @@
-/*
- * File: 0-isupper.c
- * Auth: Wendimu S Tarekegn
- */
 
 #include "main.h"
 
 /**
- * _isupper - Checks for uppercase characters.
- * @c: The character to be checked.
- *
- * Return: 1 if character is uppercase, 0 otherwise.
- */
+* _isupper -> this is a function to check upper case alphas
+* @c: an integer paramer
+* Return: something but success
+*/
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-
-	else
-		return (0);
+	return (c >= 'A' && c <= 'Z');
 }
